b45d6c0167291a9585ab4b511096f61a01984aed
sonu-kumar2001 <sonurocckkerr@gmail.com>
2020-09-15 23:48:06 +0530

    completed random generator

--- block-BJaaek/code/script.js
+++ block-BJaaek/code/script.js
@@ -2,18 +2,11 @@ let box1 = document.querySelector('.first');
 let box2 = document.querySelector('.second');
 
 function handler() {
-    let R = Math.round(Math.random()*255);
-    let G = Math.round(Math.random()*255);
-    let B = Math.round(Math.random()*255);
-
-    box1.style.backgroundColor = `rgb(${R}, ${G}, ${B})`
+    box1.style.backgroundColor = `#${Math.round(Math.random()*255)}`
+    console.log(box1.style.backgroundColor);
 }
 function handlerTwo() {
-    let R = Math.round(Math.random()*255);
-    let G = Math.round(Math.random()*255);
-    let B = Math.round(Math.random()*255);
-
-    box2.style.backgroundColor = `rgb(${R}, ${G}, ${B})`
+    box2.style.backgroundColor = `#${Math.round(Math.random()*255)}`
 }
 
 box1.addEventListener('click', handler);
